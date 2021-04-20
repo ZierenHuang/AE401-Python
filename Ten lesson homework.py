9e5d771319bfe4315abfa6a8e2b0781f30dd02da
ZierenHuang <78028434+ZierenHuang@users.noreply.github.com>
2021-04-20 19:27:32 +0800

    Add files via upload

---
+++ Ten lesson homework.py
@@ -0,0 +1,13 @@
+s = ' i am a boy'
+print(s)
+print(s.replace('am','is'))
+
+s = 'This is Pneumonoultramicroscopicsilicovolcanoconiosis it a name of illness'
+print(s.count('This'))
+print(s.count('it'))
+print(s.count('is'))
+print(s.count('illness'))
+print(s.count('name'))
+print(s.count('of'))
+print(s.count('Pneumonoultramicroscopicsilicovolcanoconiosis'))
+
